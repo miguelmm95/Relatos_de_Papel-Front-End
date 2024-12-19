3efe58658fd13694dd8e2ae16c229f3d3f81e2f4
miguelmm95 <miguelmartinezmartinez95@gmail.com>
2024-12-19 18:47:07 +0100

    Initial  commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+        <p className="footer_text">© 2024 Relatos de Papel</p>
+    </footer>
+  );
+}
